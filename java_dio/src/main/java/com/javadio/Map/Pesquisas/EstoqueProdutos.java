ad6349660a2449ec0f532bd1235b958d0bcceb13
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-10-19 14:02:06 +0100

    estudo

--- java_dio/src/main/java/com/javadio/Map/Pesquisas/EstoqueProdutos.java
+++ java_dio/src/main/java/com/javadio/Map/Pesquisas/EstoqueProdutos.java
@@ -1,5 +1,28 @@
 package com.javadio.Map.Pesquisas;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class EstoqueProdutos {
-    
+    private Map<Long, Produtos> estoqueProdutosMap; 
+    public EstoqueProdutos(){
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+    public void AdicionarProduto(long cod, String nome, int qtd, Double preco){
+        estoqueProdutosMap.put(cod, new Produtos(nome, qtd, preco));
+    }
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+
+
+
 }
